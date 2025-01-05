d8309cb92f91be892c65285ecf1b3846535410f9
arafatah <arafathowlader890@gmail.com>
2025-01-05 12:45:36 +0600

    update comments for clarity and improve function examples

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ var me = 'Jonas';
 let job = 'teacher';
 const year = 1996; */
 
-/* // functions - can't declare before initialize. Ex😔
-console.log(addDecl(5, 4));
+// functions - can't declare before initialize. Ex😔
+/* console.log(addDecl(5, 4));
 console.log(addExpr(5, 4));
-console.log(addArrow(5, 4));
+console.log(addArrow(5, 4)); */
 
 // Declaration is hoisted it will hosted to the top, and run like var variable.
 function addDecl(a, b) {
@@ -41,10 +41,9 @@ const addExpr = function (a, b) {
 const addArrow = (a, b) => {
   return a - b;
 };
-// variable declare with var create an object in window. Let and Const doesn't do this.  */
+// variable declare with var create an object in window. Let and Const doesn't do this.
 
-console.log(this)
-
+console.log(this);
 
 /* # This
 
@@ -56,3 +55,45 @@ console.log(this)
 - **Arrow Functions**: `this` is lexically bound, meaning it refers to `this` from the outer function or context where it is defined. Like if it’s define in global, then it will refer window.
 - **Event Handlers**: `this` refers to the element that triggered the event.
 - **`call()`, `apply()`, `bind()`**: These methods allow you to explicitly set the value of `this` for a function. */
+
+const Arafat = {
+  firstName: 'Arafat',
+  year: 2001,
+  calcAge: function () {
+    console.log(this);
+    console.log(2025 - this.year);
+  },
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+// It will undefined, because arrow function doesn't have his this keyword. It always indicate his outer function, it's called lexically bound.
+// Here it will refer to window.
+// Arafat.greet();
+
+const Arafat2 = {
+  firstName: 'Arafat',
+  year: 2001,
+  calcAge: function () {
+    console.log(this);
+    console.log(2025 - this.year);
+
+    // This will be undefined, because this is regular function and regular function called this always should be undefined no matter it's on an object.
+    // Some people consider this is an bug in javaScript. But this is the how it's actually work.
+    // Solution 1: Have o write the this keyword in a variable like.
+    /*  const self = this;
+    const isGenZ = function () {
+      console.log(self.year >= 1998 && self.year <= 2010);
+    };
+    isGenZ(); */
+
+    // Solution 2: Write in with array function, Because this has not his this keyword,it will always look for outer function.
+    const isGenZ = () => {
+      console.log(this.year >= 1998 && this.year <= 2010);
+    };
+    isGenZ();
+  },
+  // Fix: just write this on regular function
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+Arafat2.greet();
