1ce0cd44e3dcbc65c3cbb8b15400a44cffcfc852
arafatah <arafathowlader890@gmail.com>
2025-01-05 11:42:41 +0600

    update comments for clarity on variable declaration and 'this' context

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var me = 'Jonas';
 let job = 'teacher';
 const year = 1996; */
 
-// functions - can't declare before initialize. Ex😔
+/* // functions - can't declare before initialize. Ex😔
 console.log(addDecl(5, 4));
 console.log(addExpr(5, 4));
 console.log(addArrow(5, 4));
@@ -41,3 +41,18 @@ const addExpr = function (a, b) {
 const addArrow = (a, b) => {
   return a - b;
 };
+// variable declare with var create an object in window. Let and Const doesn't do this.  */
+
+console.log(this)
+
+
+/* # This
+
+- **Global Context**: `this` refers to the global object (`window` in browsers, `global` in Node.js).
+- **Function (Non-strict Mode)**: `this` refers to the global object, unless explicitly called with a different context.
+- **Strict Mode**: `this` is `undefined` in functions unless bound to an object.
+- **Object Method**: `this` refers to the object that the method belongs to / means owner of the object.
+- **Constructor/Class**: `this` refers to the new instance created by the constructor or class.
+- **Arrow Functions**: `this` is lexically bound, meaning it refers to `this` from the outer function or context where it is defined. Like if it’s define in global, then it will refer window.
+- **Event Handlers**: `this` refers to the element that triggered the event.
+- **`call()`, `apply()`, `bind()`**: These methods allow you to explicitly set the value of `this` for a function. */
